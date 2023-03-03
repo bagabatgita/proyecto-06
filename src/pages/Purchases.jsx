fd65d15ce17d2021d2f2eb4a169910dca88df798
bagabatgita <60152982+bagabatgita@users.noreply.github.com>
2023-03-02 23:08:36 -0500

    Update Purchases.jsx

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import PurchaseCard from "../components/purchases/PurchaseCard";
+import PurchaseCard from "../components/Purchases/PurchaseCard";
 import { axiosEcommerce, getConfig } from "../utils/configAxios";
 
 const Purchases = () => {
